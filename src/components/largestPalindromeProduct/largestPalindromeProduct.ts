5062c195bda8ef7525ccb323f922817e52c5cde3
Piotr Ilski <piotr.ilski@gmail.com>
2015-09-10 10:21:09 +0200

    problem 4 solved

--- src/components/largestPalindromeProduct/largestPalindromeProduct.ts
+++ src/components/largestPalindromeProduct/largestPalindromeProduct.ts
@@ -4,20 +4,35 @@ import {Component, View, bootstrap, CORE_DIRECTIVES} from 'angular2/angular2';
 	selector: 'largest-palindrome-product'
 })
 @View({
-	templateUrl: './components/largestPalindromeProduct/largestPalindromeProduct.html'	
+	templateUrl: './components/largestPalindromeProduct/largestPalindromeProduct.html',
+	directives: [CORE_DIRECTIVES]	
 })
 
 export class LargestPalindromeProduct {
 	isNumberPalindrome : boolean;
+	largestPalindrome : number;
+	palindromes : Array<PalindromeProduct>;
 	
 	constructor() {
-		this.isNumberPalindrome = false;
+		this.isNumberPalindrome = false;		
+		this.palindromes = [];
+	}
+	
+	getLargestPalindrome(numberOfDigits : number) {		
+		this.palindromes = this.getListOfProductsMadeOfANumberOfDigits(
+			numberOfDigits, 
+			(nmb) => this.isPalindrome(nmb));
+
+		let numbers = this.palindromes.map((p) => {
+			return p.palindrome;
+		});			
+		
+		this.largestPalindrome = Math.max(...numbers);
 	}
 	
 	isGivenNumberPalindrome(givenNumber: number) {
 		this.isNumberPalindrome = this.isPalindrome(givenNumber);		
-	}
-	
+	}	
 	
 	private reverse(givenString : string) : string {
 		if (givenString.length < 2) {
@@ -47,11 +62,46 @@ export class LargestPalindromeProduct {
 		return firstHalf === secondHalf;
 	}
 	
-	private getListOfProductsMadeOfANumberOfDigits(numberOfDigits : number) : Array<number> {
-		let list : Array<number> = [];
+	private getListOfProductsMadeOfANumberOfDigits(
+		numberOfDigits : number, 
+		canAdd: (number) => boolean) : Array<PalindromeProduct> {
 		
+		let list : Array<PalindromeProduct> = [];
 		
+		var startValue = Math.pow(10, numberOfDigits - 1);
+		var endValue = Math.pow(10, numberOfDigits);
+		
+		for(let f = startValue; f < endValue; f++) {
+			let pow = f*f;
+			if(canAdd(pow)) {
+				list.push(new PalindromeProduct(f,f,pow));
+			}
+			
+			if(f+1 === endValue) {
+				break;
+			} else {
+				for(let s = f+1; s < endValue; s++)
+				{
+					let nmb = f*s;
+					if(canAdd(nmb)) {
+						list.push(new PalindromeProduct(f,s, f*s));
+					}
+				}
+			}
+		}
 		
 		return list;
+	}
+}
+
+class PalindromeProduct {
+	palindrome : number;
+	firstFactor : number;
+	secondFactor: number;
+	
+	constructor(firstFactor : number, secondFactor : number, palindrome : number) {
+		this.firstFactor = firstFactor;
+		this.secondFactor = secondFactor;
+		this.palindrome = palindrome;		
 	}
 }
